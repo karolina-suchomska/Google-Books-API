1e90bece0eda534b1ff3e6c7fd2d87291e5dbe8d
unknown <xrage6@gmail.com>
2019-04-14 12:43:47 +0200

    Updates

---
+++ precache-manifest.19a2f8d48e6d72105e7a1f832d48aa46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1deab6a8cc3dee14348b",
+    "url": "/Google-Books-API/static/js/runtime~main.a027c759.js"
+  },
+  {
+    "revision": "ec522f65c20386f30447",
+    "url": "/Google-Books-API/static/js/main.2fe77ec4.chunk.js"
+  },
+  {
+    "revision": "7a44d8547ae1187dd69a",
+    "url": "/Google-Books-API/static/js/2.af534fb8.chunk.js"
+  },
+  {
+    "revision": "ec522f65c20386f30447",
+    "url": "/Google-Books-API/static/css/main.c55495cc.chunk.css"
+  },
+  {
+    "revision": "362ca4abc29b96b7164416a8592cd913",
+    "url": "/Google-Books-API/index.html"
+  }
+];
