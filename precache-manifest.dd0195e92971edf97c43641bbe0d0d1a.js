7379c155da7a16b041213d74dcd3c916776be7f5
unknown <xrage6@gmail.com>
2019-06-11 01:26:53 +0200

    Updates

---
+++ precache-manifest.dd0195e92971edf97c43641bbe0d0d1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eec73610911a7e5030d8463800dc7f40",
+    "url": "/Google-Books-API/index.html"
+  },
+  {
+    "revision": "97f5ed995c12b9e1a848",
+    "url": "/Google-Books-API/static/css/main.7a9c78da.chunk.css"
+  },
+  {
+    "revision": "6bde06bbbffddd21abd0",
+    "url": "/Google-Books-API/static/js/2.2f8137ed.chunk.js"
+  },
+  {
+    "revision": "97f5ed995c12b9e1a848",
+    "url": "/Google-Books-API/static/js/main.4a600c07.chunk.js"
+  },
+  {
+    "revision": "1deab6a8cc3dee14348b",
+    "url": "/Google-Books-API/static/js/runtime~main.a027c759.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Google-Books-API/precache-manifest.19a2f8d48e6d72105e7a1f832d48aa46.js"
+  "/Google-Books-API/precache-manifest.dd0195e92971edf97c43641bbe0d0d1a.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Google-Books-API/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Google-Books-API/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
