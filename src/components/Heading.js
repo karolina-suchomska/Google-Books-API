0ed9174e16a72405222efff82895a2d078997525
unknown <xrage6@gmail.com>
2019-06-11 01:17:21 +0200

    Change HTML element in Heading

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Heading = () => {
     return ( 
-        <div className="heading">
+        <header>
             <h1>Google Books Search</h1>
-        </div>
+        </header>
      )
 }
  
