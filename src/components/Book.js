5402ae46382368c7938062aa883e7da536aef854
unknown <xrage6@gmail.com>
2019-06-11 01:04:49 +0200

    Change HTML element in Books.js

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
 const Book = (props) => { 
-    
     return ( 
         <div className="card">
             <img src={props.image} alt="" />
-            <div className="desc">
+            <section className="desc">
                 <h2>{props.title}</h2>
                 <p>{((props.description) ? (props.description.substr(0, 55)+"...") : props.description)}</p>
-            </div>
+            </section>
         </div>
     );
 }
